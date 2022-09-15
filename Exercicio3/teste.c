a8a3630f2927514e81aac90bccd743aa92c8956a
Paulo Cesar <pauloreispassos988@gmail.com>
2022-09-15 13:19:41 -0300

    15/09

--- Exercicio3/teste.c
+++ Exercicio3/teste.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-
-int main(int argc, char const *argv[])
+//#include <conio.h>
+#include <stdlib.h>
+int main(void)
 {
-    
-    return 0;
+  int i;
+  
+  printf("Gerando 10 valores aleatorios:\n\n");
+  
+  for (i = 0; i < 10; i++)
+  {
+    /* gerando valores aleatórios entre zero e 100 */
+    printf("%d ", rand() % 100);
+  }
+  
+  return 0;
 }
