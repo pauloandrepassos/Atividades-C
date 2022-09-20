9aaec187d6bcd27664de59c743aa12d63216e8fb
Paulo Cesar <pauloreispassos988@gmail.com>
2022-09-20 09:10:35 -0300

    20/09

--- Atividade4/Q12a.c
+++ Atividade4/Q12a.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    
+    int num, i;
+    printf("Digite um número: ");
+    scanf(" %d", &num);
+
+    printf("O numero %d é fatorado pelos numeros ", num);
+
+    for(i = 2; num > 1; i++) {
+        while(num % i == 0) {
+            printf(" %d", i);
+            num /= i;
+        }
+    }
 }
