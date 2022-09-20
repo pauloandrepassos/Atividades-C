32c7b67fca45b0edc51512dfd6d4b0e7f369827e
Paulo Cesar <pauloreispassos988@gmail.com>
2022-09-20 12:50:49 -0300

    20/09

--- Atividade4/Q15.c
+++ Atividade4/Q15.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int alt, sub, des, dia;
-    alt = 10000;
-    sub = 100;
-    des = 50;
-    dia = 0;
-     while(alt > 0) {
-        dia++;
-        alt -= sub - des;
-     }
-     printf("Serao necessario %d dias.", dia);
+    
 
      return 0;
 }
